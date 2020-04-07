5f1ec22bd6ada3aabc64fa3a1d0d14807d90788d
Vadim Toropoff <vadimtoropoff@gmail.com>
2020-04-07 23:03:46 +0200

    dernière version

--- exo_21.rb
+++ exo_21.rb
@@ -1,10 +1,12 @@
 puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
 print ">"
 etage=gets.chomp.to_i
+
 puts "Voici la pyramide :"
 
 etage.times do |i| # pour faire la boucle des étages
   i += 1
   i < etage
-  puts "#"*i
+  print " "*(etage-i) # création d'un espace
+  puts "#"*(i)
 end
